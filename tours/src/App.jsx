331d9c045f200ef6374a433562148dfa9ea64713
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-14 19:57:14 +0530

    changes

--- tours/src/App.jsx
+++ tours/src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+
 const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const fetchAPI = await fetch(url);
+      const res = await fetchAPI.json();
+      setTours(res);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
 
   return <h2>Tours Starter</h2>;
 };
