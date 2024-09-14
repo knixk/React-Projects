47045954ee44510c5f94c89dd9269e86e3059d64
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-14 19:45:02 +0530

    changes

--- tours/src/App.jsx
+++ tours/src/App.jsx
@@ -1,6 +1,9 @@
 const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
